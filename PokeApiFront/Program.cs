798bb1b3304f131705ca985be7c1a8f9099029fe
Pol Hernan <pol_hernancamino@iescarlesvallbona.cat>
2024-12-10 19:30:08 +0100

    Errors Solved

--- PokeApiFront/Program.cs
+++ PokeApiFront/Program.cs
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 ﻿using PokeApiFront.Model;
-=======
-﻿using PokeApiBack.Repositories;
->>>>>>> origin/main
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,27 +10,13 @@ namespace PokeApiFront
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static async Task Main()
         {
-            /*Application.EnableVisualStyles();
+            Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
             new FrontController();
             Application.Run();
-=======
-            Application.Run(new Form1());*/
-            await GetPokemons();
-        }
-
-        public static async Task GetPokemons()
-        {
-            var res = await PokemonRepository.GetAllPokemonsByName("gardevoir");
-            return;
->>>>>>> origin/main
         }
     }
 }
